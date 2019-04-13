9f91d1ed7f6d98a33cded29857b0bcc595ffe4d9
Sandip Nirmal <sandipnirmal@icloud.com>
2019-04-13 21:53:30 +0530

    modified UI

--- src/modules/weather/CityInfo.js
+++ src/modules/weather/CityInfo.js
@@ -5,7 +5,10 @@ import { Typography } from './../../components'
 export const CityInfo = React.memo(({info: {city, region, country}}) => {
     return (
       <div className={'flex center'}>
-        <Typography variant='h5'>{city} ({region.trim()}), {country}</Typography>
+        <Typography variant='h4'>
+          {city} 
+        {/* ({region.trim()}), {country} */}
+        </Typography>
         {/* <div>01/04, 8:45 AM</div> */}
       </div>
     )
--- src/modules/weather/CurrentWeather.js
+++ src/modules/weather/CurrentWeather.js
@@ -1,22 +1,31 @@
 import React from 'react'
 
-import { Typography } from './../../components'
+import {Typography} from './../../components'
 import * as Cond from './../../resources/icons'
-import { farenheitToCelsius, getConditionIcon } from './../../services'
+import {farenheitToCelsius, getConditionIcon} from './../../services'
 
-export const CurrentWeather = React.memo(({weather: {text, code, temperature}}) => {
+export const CurrentWeather = React.memo(({
+  weather: {
+    text,
+    code,
+    temperature
+  }
+}) => {
   return (
-    <div>
-      <div className='flex' style={{alignItems: 'center'}}>
-        <img src={Cond[`Cond_${getConditionIcon(code)}`]} alt={text} />
-        <Typography variant='h4'>{text}</Typography>
-      </div>
-      {/* <div>
-        37<sup>o</sup>
-        20<sup>o</sup>
-      </div> */}
-      <div style={{paddingLeft: 16}}>
-        <Typography variant='h2'>{farenheitToCelsius(temperature)}&#xb0;C</Typography>
+    <div style={{
+      margin: `32px 0`
+    }}>
+      <div className='flex center' style={{
+        flexDirection: 'column'
+      }}>
+        <Typography variant='h5'>{text}</Typography>
+        <div className='flex center'>
+          <img
+            src={Cond[`Cond_${getConditionIcon(code)}`]}
+            alt={text}
+            className='cond-code'/>
+          <Typography variant='h2'>{farenheitToCelsius(temperature)}&#xb0;</Typography>
+        </div>
       </div>
     </div>
   )
