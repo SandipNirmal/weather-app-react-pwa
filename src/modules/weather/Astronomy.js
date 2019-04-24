72a331dbc72289a339e88d1bc2109cf3873e946e
Sandip Nirmal <sandipnirmal@icloud.com>
2019-04-24 08:35:28 +0530

    Added react hooks instead of class component.
    1. Converted Astronomy component from a class component to use react
    hooks.

--- src/modules/weather/Astronomy.js
+++ src/modules/weather/Astronomy.js
@@ -1,57 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import {Card, Typography} from '../../components'
 import {drawAstroImage} from './../../services'
 
-export class Astronomy extends React.PureComponent {
+export const Astronomy = ({ data: { sunrise, sunset }}) => {
+  useEffect(() => drawAstroImage(sunrise, sunset), [])
 
-  componentDidMount() {
-    const {
-      props: {
-        data: {
-          sunrise,
-          sunset
-        }
-      }
-    } = this
-    drawAstroImage(sunrise, sunset)
-  }
+  return (
+    <div className='astrology'>
+      <Card>
+        <div className='title'>
+          <Typography
+            variant='subtitle1'
+            style={{
+            textTransform: 'uppercase'
+          }}>Sun &amp; Moon</Typography>
+        </div>
 
-  render() {
-    const {
-      props: {
-        data: {
-          sunrise,
-          sunset
-        }
-      }
-    } = this
-    return (
-      <div className='astrology'>
-        <Card>
-          <div className='title'>
-            <Typography
-              variant='subtitle1'
-              style={{
-              textTransform: 'uppercase'
-            }}>Sun &amp; Moon</Typography>
+        <div className='flex astrology-detail'>
+          <div className="sun-pos">
+            <canvas id="astro-img" width="200" height="120"></canvas>
           </div>
 
-          <div className='flex astrology-detail'>
-            <div className="sun-pos">
-              <canvas id="astro-img" width="200" height="120"></canvas>
-            </div>
-
-            <div className='flex' style={{flexDirection: 'column'}}>
-              <Typography variant='subtitle3'>Sunrise</Typography>
-              <Typography variant='caption'>{sunrise}</Typography>
-              <Typography variant='subtitle3'>Sunset</Typography>
-              <Typography variant='caption'>{sunset}</Typography>
-            </div>
+          <div className='flex' style={{flexDirection: 'column'}}>
+            <Typography variant='subtitle3'>Sunrise</Typography>
+            <Typography variant='caption'>{sunrise}</Typography>
+            <Typography variant='subtitle3'>Sunset</Typography>
+            <Typography variant='caption'>{sunset}</Typography>
           </div>
+        </div>
 
-        </Card>
-      </div>
-    )
-  }
+      </Card>
+    </div>
+  )
 }
