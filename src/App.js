916883764c86469c7c6f45febdef3951ca22bf7f
Sandip Nirmal <sandipnirmal@icloud.com>
2019-05-09 22:30:43 +0530

    Updated button variant

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import { getCurrentLocation } from './services'
+import React, { useState, useEffect } from 'react';
+import Typography from 'typography-material'
+import { getCurrentLocation } from './services';
 import { Loading } from './components';
 
 import './App.css';
@@ -11,18 +12,34 @@ function App() {
 
   useEffect(() => {
     const fetchLocation = async () => {
-      const {lat, lon} = await getCurrentLocation()
-      setLocation({lat, lon})
+      const { lat, lon } = await getCurrentLocation();
+      setLocation({ lat, lon });
     };
 
-    fetchLocation()
+    fetchLocation();
 
     return () => {
       // Clean up here
     };
   }, []);
 
-  return location.lat ? <Weather location={location} /> : <Loading />
+  return (
+    <div>
+      <div className='header'>
+        <div className='add-city'>
+          <Typography onClick={() => alert('Add New City')} variant='button'>Add City</Typography>
+        </div>
+      </div>
+
+      {location.lat ? (
+        <div className="slider">
+          <Weather location={location} />
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </div>
+  );
 }
 
 export default App;
